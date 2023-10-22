880000563218e2d67bfdb96f5d555ab52402da9e
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-10-22 23:24:34 +0900

    計算アプリ課題完了

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class CalcController extends Controller
 {
-    //
+    public function result($num1,$operator,$num2)
+    {
+        $result = 0;
+        if ($operator == "addition"){
+            $result = $num1 + $num2;
+        }elseif($operator == "subtraction"){
+            $result = $num1 - $num2;
+        }elseif($operator == "multiplication"){
+            $result = $num1 * $num2;
+        }elseif($operator == "division"){
+            $result = $num1 / $num2;
+        }
+//return $result;
+//echo "{$result}";
+        return view('message.calculation', ['result' => $result]);
+    }
 }
